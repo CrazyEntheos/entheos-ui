139bf98597bbcfa970004073f74942a1bac532a4
Chitakala <yc5587@intl.att.com>
2018-08-02 13:34:15 +0530

    [GUI-010] - Environment file parameters added to reflect the local url paramenters

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
+import { environment } from '../environments/environment'
 
 import { Observable } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -17,7 +18,7 @@ import { MessageService } from './message.service';
 export class ProductService {
 
   private productsUrl = 'api/products';  // URL to web api
-  url = 'http://localhost:9080/fashion/api';
+  url = environment.apiUrl;
   //http://localhost:9080/fashion/api/categories/5000001
   //http://localhost:9080/fashion/api/categories/5000001/products
   //http://localhost:9080/fashion/api/orders
